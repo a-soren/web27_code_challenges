0433d51046f883024856dc67ea8a293cd51247c2
Amberly Sorensen <47939620+a-soren@users.noreply.github.com>
2019-11-21 19:03:16 -0700

    Add files via upload

---
+++ web27_code_challenges/JavaScript/task_function.js
@@ -0,0 +1,17 @@
+// write a function that explains a daily task, be creative and think of methods
+// the methods don't have to actually work in the function, but be aware of what they do
+// get familiar with the syntax and start to "think like a programmer!"
+
+// function brushTeeth(toothpaste, toothbrush) {
+//   if (!toothpaste) {
+//     return `You should go buy some toothpaste!`;
+//   }
+//   if (!toothbrush) {
+//     return `You should go buy a toothbrush!`;
+//   } else {
+//     return `Hey, you should brush your teeth!`;
+//   }
+// }
+
+// brushTeeth(crest, quip);
+// console.log(brushTeeth);
